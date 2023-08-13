30cc1b2ae1c95990a5b4333f1d573b7440cc10b9
manishsinghmodi <manishsahu43962@gmail.com>
2023-08-13 12:20:13 +0530

    first push

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { useRef, useState } from "react"
 import { youtube_parser } from "./utils";
+import Footer from './Footer';
+
 
 function App() {
+
   const inputUrlRef = useRef();
   const [urlResult, setUrlResult] = useState(null);
 
@@ -31,23 +34,47 @@ function App() {
 
   return (
     <div className="app">
-      <span className="logo">youtube2mp3</span>
+      
+      <span className="logo"></span>
       <section className="content">
         <h1 className="content_title">YouTube to MP3 Converter</h1>
         <p className="content_description">
           Transform YouTube videos into MP3s in just a few clicks!
         </p>
+        
 
         <form onSubmit={handleSubmit} className="form">
-          <input ref={inputUrlRef} placeholder="Paste a Youtube video URL link..." className="form_input" type="text" />
-          <button type="submit" className="form_button">Search</button>
+        <label for="myInput" class="label">
+        <span class="label-title">Enter url to search</span>
+        <input id="myInput" class="input" name="text" ref={inputUrlRef} placeholder="Paste a Youtube video URL link..." type="text"/>
+        </label>
+
+       <div className="div-btn">
+       <button class="cssbuttons-io-button"> search
+        <div class="icon">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
+        </div>
+        </button>
+       </div>
+
         </form>
 
         {urlResult ? <a target='_blank' rel="noreferrer" href={urlResult} className="download_btn">Download MP3</a> : ''}
+
+        <Footer/>
+
+
+        
         
       </section>
+
+      
+
+      
     </div>
   )
 }
 
 export default App
+
+
---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {BsCodeSlash} from 'react-icons/bs';
+
+function Footer (){
+    return (
+
+        <div className="foter">
+
+        <div className="footer ">
+            <BsCodeSlash className="f-code"/>
+            <div className="f-text">by Manish Sahu</div>
+        </div>
+
+        </div>
+    );
+}
+
+
+export default Footer;
